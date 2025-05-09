014aa928945d6b5e175a00f9d59f53bfe87e0d1b
Sarina Etminan <Sarina@Mac-air.lan>
2025-05-09 16:38:15 -0700

    WIP: script updates before rebase

---
+++ code/api/fetch_who_inactivity.R
@@ -0,0 +1,6 @@
+# Title: Fetch WHO Physical Inactivity Data – National Health and Data Inquiry 2025
+# Author: Sarina Etminan
+
+# Description:
+# Uses the WHO GHO API to fetch country-level physical inactivity prevalence (adults)
+# and saves the raw dataset to data/raw/who_inactivity.csv
